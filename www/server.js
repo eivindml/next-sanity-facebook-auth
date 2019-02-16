a1253605f254dd3dbebf7edaa08f23dd060cc5ce
Eivind Mikael Lindbråten <eivindml@icloud.com>
2019-02-16 09:52:13 +0100

    Adds node server for routing

---
+++ www/server.js
@@ -0,0 +1,46 @@
+const next = require('next')
+const { parse } = require('url')
+const httpProxy = require('http-proxy')
+const { createServer } = require('http')
+
+// TODO: Go through this and clean up and understand
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const handle = app.getRequestHandler()
+const proxy = httpProxy.createProxyServer()
+
+app.prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
+
+      switch (pathname) {
+        case '/':
+          app.render(req, res, '/', query)
+          break
+        case '/login':
+          app.render(req, res, '/login', query)
+          break
+        case '/api/login.js':
+          proxy.web(req, res, { target: 'http://localhost:3001' }, error =>
+            console.log('Error!', error)
+          )
+          break
+        case '/profile':
+          app.render(req, res, '/profile', query)
+          break
+        case '/api/profile.js':
+          proxy.web(req, res, { target: 'http://localhost:3001' }, error =>
+            console.log('Error!', error))
+          break
+        default:
+          handle(req, res, parsedUrl)
+          break
+      }
+    })
+      .listen(3000, err => {
+        if (err) { throw err }
+        console.log('> Ready on http://localhost:3000')
+      })
+  })
