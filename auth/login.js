0a87e71f10016342848cc4509b57f2cb26f79dc5
Eivind Mikael Lindbråten <eivindml@icloud.com>
2019-02-17 11:36:13 +0100

    Refactor into functions

--- auth/login.js
+++ auth/login.js
@@ -1,102 +1,74 @@
 require('dotenv').config()
-
-const { send, run, json } = require('micro')
+const { send, run, json, createError } = require('micro')
 const fetch = require('isomorphic-unfetch')
 const sanity = require('@sanity/client')
 
 const login = async (req, res) => {
+  // TODO: Return values explicty and infer them
   const { code } = await json(req)
-  const FACEBOOK_REDIRECT_URI = 'http://localhost:3000/callback'
+  const facebook = await getFacebookToken(code)
+  const profile = await getFacebookProfile(facebook.access_token)
 
-  const response = await fetch(`https://graph.facebook.com/v3.2/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${FACEBOOK_REDIRECT_URI}&client_secret=${process.env.FACEBOOK_CLIENT_SECRET}&code=${code}`, {
-    method: 'GET'
+  const expires = new Date()
+  expires.setSeconds(expires.getSeconds() + facebook.expires_in)
+
+  const sanityUser = await getSanityUser(profile.id, profile.name, expires.toISOString())
+
+  await createUser(sanityUser.token, profile.id, profile.name)
+
+  send(res, 200, {
+    token: sanityUser.token,
+    image: profile.picture.data.url,
+    expire: expires.toISOString()
+  })
+}
+
+const getFacebookToken = async (code) => {
+  const FACEBOOK_REDIRECT_URI = 'http://localhost:3000/callback'
+  const url = `https://graph.facebook.com/v3.2/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${FACEBOOK_REDIRECT_URI}&client_secret=${process.env.FACEBOOK_CLIENT_SECRET}&code=${code}`
+  try {
+    const response = await fetch(url)
+    return response.json()
+  } catch (error) {
+    throw createError(error.statusCode, error.statusText)
+  }
+}
+
+const getFacebookProfile = async (token) => {
+  const response = await fetch(`https://graph.facebook.com/me?fields=id,name,picture.width(512)&access_token=${token}`)
+  return response.json()
+}
+
+const getSanityUser = async (id, name, expires) => {
+  const response = await fetch(`https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v1/auth/thirdParty/session`, {
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${process.env.SANITY_TOKEN_CREATE_SESSION}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      userId: `e-${id}`,
+      userFullName: name,
+      sessionExpires: expires
+    })
+  })
+  return response.json()
+}
+
+const createUser = async (token, id, name) => {
+  const client = sanity({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET,
+    token: token
   })
 
-  if (response.ok) {
-    const json = await response.json()
-    console.log('Success')
-    console.log(json.access_token)
-    const re = await fetch(`https://graph.facebook.com/me?fields=id,name,picture.width(512)&access_token=${json.access_token}`, {
-      method: 'GET'
-    })
-    if (re.ok) {
-      const js = await re.json()
-      console.log('sucessfully got me')
-      console.log(js)
-
-      const expireDate = new Date()
-      expireDate.setSeconds(expireDate.getSeconds() + json.expires_in)
-
-      const a = await fetch(`https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v1/auth/thirdParty/session`, {
-        method: 'post',
-        headers: {
-          'Authorization': `Bearer ${process.env.SANITY_TOKEN_CREATE_SESSION}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          // Both fields are required
-          userId: `e-${js.id}`,
-          userFullName: js.name,
-          sessionExpires: expireDate.toISOString()
-        })
-      })
-      //
-      const sanityUser = await a.json()
-
-      console.log('sanity')
-      console.log(sanityUser.token)
-
-      let s = sanity({
-        projectId: process.env.SANITY_PROJECT_ID,
-        dataset: process.env.SANITY_DATASET,
-        token: sanityUser.token
-      })
-
-      const doc = {
-        _type: 'user',
-        _id: `e-${js.id}`,
-        username: js.name
-      }
-
-      s.createIfNotExists(doc).then(res => {
-        console.log(`User was created, document ID is ${res._id}`)
-        console.log(res)
-      })
-
-      // TODO: Here we need to create, login user.
-      // And set expiry to same as facebook token.
-
-      // TODO: Trenger ikke sende fb token,
-      // men send sanity token + expirey date,
-      // slik at cookie kan settes.
-      send(res, 200, {
-        token: sanityUser.token,
-        expire: expireDate.toISOString(),
-        // token: sanityUser.token,
-        image: js.picture.data.url
-      })
-    }
-
-    send(res, 200, { token: json.access_token })
-  } else {
-    console.log('Error')
-    console.log(response)
-    send(res, response.status, 'Error')
+  const doc = {
+    _type: 'user',
+    _id: `e-${id}`,
+    username: name
   }
-  // try {
-  //   const response = await fetch(url)
-  //   if (response.ok) {
-  //     console.log('asdkasdk')
-  //     const { id } = await response.json()
-  //     send(res, 200, { token: id })
-  //   } else {
-  //     console.log('doh')
-  //     send(res, response.status, response.statusText)
-  //   }
-  // } catch (error) {
-  //   console.log('dohasdasd')
-  //   throw createError(error.statusCode, error.statusText)
-  // }
+
+  return client.createIfNotExists(doc)
 }
 
 module.exports = (req, res) => run(req, res, login)
