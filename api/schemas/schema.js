5ee0515edad57a8df6e41cbf7b1044645a0758ec
Eivind Mikael Lindbråten <eivindml@icloud.com>
2019-02-17 11:36:23 +0100

    Clean up file

--- api/schemas/schema.js
+++ api/schemas/schema.js
@@ -1,6 +1,5 @@
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
-
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
@@ -8,15 +7,13 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import user from './user'
 import note from './note'
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: 'default',
   // Then proceed to concatenate our our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
+    // The following are document types
+    // which will appear in the studio
     user,
     note
   ])
